c9ddb026faa401e61cd6ccc72320362af2b82814
oliver <olivercastroteran@gmail.com>
2020-12-14 13:22:22 -0400

    banner component added

--- src/languages/english.js
+++ src/languages/english.js
@@ -7,6 +7,13 @@ const english = {
   },
   sidebar: ['Dashboard', 'Balance', 'Debts', 'Stock'],
   header: 'Search',
+  dashboard: {
+    banner: {
+      welcomeTitle: 'Welcome',
+      welcomeTxt:
+        'FinPal the best app to manage your buisness finances, control your incomes, expenses, debts and stock in one place. And Block your account whenever you need',
+    },
+  },
 };
 
 export default english;
--- src/views/dashboard/components/banner/Banner.js
+++ src/views/dashboard/components/banner/Banner.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
 import './Banner.scss';
+import { useSelector } from 'react-redux';
+import { WalletImg } from '../../../../assets/images';
+import { english, spanish } from '../../../../languages';
 
 const Banner = () => {
+  const language = useSelector((state) => state.settings.language);
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    if (language === 'english') {
+      setContent({ ...english.dashboard.banner });
+    } else if (language === 'spanish') {
+      setContent({ ...spanish.dashboard.banner });
+    }
+    // eslint-disable-next-line
+  }, [language]);
+
   return (
     <div className="banner">
-      <h1>Banner</h1>
+      <div className="banner__left">
+        <h2>{content?.welcomeTitle} Oliver</h2>
+        <p>{content?.welcomeTxt}</p>
+      </div>
+      <div className="banner__right">
+        <WalletImg />
+      </div>
     </div>
   );
 };
